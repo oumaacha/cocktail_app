cc78f52cfdc2ef528c144d0c560628aaedb103f9
HifiSaba <safihiba1997@gmail.com>
2023-12-08 20:01:07 +0100

    Initialize category screen

---
+++ app/src/main/java/fr/enseirb/cocktail_app/view/screen/CategoryScreen.kt
@@ -0,0 +1,18 @@
+package fr.enseirb.cocktail_app.View.screen
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun CategoryScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ){
+        //Code à ajouter
+    }
+}
